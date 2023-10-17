121062bd9adc982d79ed002cbccc33130e530f65
david0411 <tlyeyd@gmail.com>
2023-10-18 04:04:15 +0800

    LightGCN Class

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-import torch
-import pandas as pd
-from sklearn import preprocessing as pp
-from sklearn.model_selection import train_test_split
-import scipy.sparse as sp
-import numpy as np
 import random
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
 import time
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+import torch
+import torch.nn as nn
+from sklearn import preprocessing as pp
 from tqdm.notebook import tqdm
 
 pd.set_option('display.max_colwidth', None)
@@ -77,7 +75,7 @@ def get_metrics(user_Embed_wts, item_Embed_wts, n_users, n_items, train_data, te
     topk_relevance_score = torch.topk(relevance_score, K).values
     topk_relevance_indices = torch.topk(relevance_score, K).indices
 
-    topk_relevance_indices_df = pd.DataFrame(topk_relevance_indices.numpy(), 
+    topk_relevance_indices_df = pd.DataFrame(topk_relevance_indices.numpy(),
                                              columns=['top_indx_' + str(x + 1) for x in range(K)])
 
     topk_relevance_indices_df['user_ID'] = topk_relevance_indices_df.index
@@ -122,6 +120,7 @@ def get_metrics(user_Embed_wts, item_Embed_wts, n_users, n_items, train_data, te
     return metrics_df['recall'].mean(), metrics_df['precision'].mean(), metrics_df['ndcg'].mean(), metrics_df[
         'map'].mean()
 
+
 class LightGCN(nn.Module):
     def __init__(self, data, n_users, n_items, n_layers, latent_dim):
         super(LightGCN, self).__init__()
@@ -139,7 +138,7 @@ class LightGCN(nn.Module):
         self.E0.weight = nn.Parameter(self.E0.weight)
 
     def get_A_tilda(self):
-        R = sp.dok_matrix((self.n_users, self.n_items), dtype = np.float32)
+        R = sp.dok_matrix((self.n_users, self.n_items), dtype=np.float32)
         R[self.data['user_idx'], self.data['item_idx']] = 1.0
 
         adj_mat = sp.dok_matrix(
@@ -148,8 +147,8 @@ class LightGCN(nn.Module):
         adj_mat = adj_mat.tolil()
         R = R.tolil()
 
-        adj_mat[: n_users, n_users :] = R
-        adj_mat[n_users :, : n_users] = R.T
+        adj_mat[: n_users, n_users:] = R
+        adj_mat[n_users:, : n_users] = R.T
         adj_mat = adj_mat.todok()
 
         rowsum = np.array(adj_mat.sum(1))
@@ -182,7 +181,7 @@ class LightGCN(nn.Module):
             all_layer_embedding.append(E_lyr)
 
         all_layer_embedding = torch.stack(all_layer_embedding)
-        mean_layer_embedding = torch.mean(all_layer_embedding, axis = 0)
+        mean_layer_embedding = torch.mean(all_layer_embedding, axis=0)
 
         final_user_Embed, final_item_Embed = torch.split(mean_layer_embedding, [n_users, n_items])
         initial_user_Embed, initial_item_Embed = torch.split(self.E0.weight, [n_users, n_items])
@@ -193,10 +192,153 @@ class LightGCN(nn.Module):
         final_user_Embed, final_item_Embed, initial_user_Embed, initial_item_Embed = self.propagate_through_layers()
 
         users_emb, pos_emb, neg_emb = final_user_Embed[users], final_item_Embed[pos_items], final_item_Embed[neg_items]
-        userEmb0,  posEmb0, negEmb0 = initial_user_Embed[users], initial_item_Embed[pos_items], initial_item_Embed[neg_items]
+        userEmb0, posEmb0, negEmb0 = initial_user_Embed[users], initial_item_Embed[pos_items], initial_item_Embed[
+            neg_items]
 
-        return users_emb, pos_emb, neg_emb, userEmb0,  posEmb0, negEmb0
+        return users_emb, pos_emb, neg_emb, userEmb0, posEmb0, negEmb0
 
 
 lightGCN = LightGCN(train_df, n_users, n_items, n_layers, latent_dim)
 print("Size of Learnable Embedding : ", list(lightGCN.parameters())[0].size())
+
+
+def bpr_loss(users, users_emb, pos_emb, neg_emb, userEmb0, posEmb0, negEmb0):
+    reg_loss = (1 / 2) * (userEmb0.norm().pow(2) +
+                          posEmb0.norm().pow(2) +
+                          negEmb0.norm().pow(2)) / float(len(users))
+    pos_scores = torch.mul(users_emb, pos_emb)
+    pos_scores = torch.sum(pos_scores, dim=1)
+    neg_scores = torch.mul(users_emb, neg_emb)
+    neg_scores = torch.sum(neg_scores, dim=1)
+
+    loss = torch.mean(torch.nn.functional.softplus(neg_scores - pos_scores))
+
+    return loss, reg_loss
+
+
+def data_loader(data, batch_size, n_usr, n_itm):
+    interected_items_df = data.groupby('user_idx')['item_idx'].apply(list).reset_index()
+
+    def sample_neg(x):
+        while True:
+            neg_id = random.randint(0, n_itm - 1)
+            if neg_id not in x:
+                return neg_id
+
+    indices = [x for x in range(n_usr)]
+
+    if n_usr < batch_size:
+        users = [random.choice(indices) for _ in range(batch_size)]
+    else:
+        users = random.sample(indices, batch_size)
+
+    users.sort()
+
+    users_df = pd.DataFrame(users, columns=['users'])
+
+    interected_items_df = pd.merge(interected_items_df, users_df, how='right', left_on='user_idx', right_on='users')
+
+    pos_items = interected_items_df['item_idx'].apply(lambda x: random.choice(x)).values
+
+    neg_items = interected_items_df['item_idx'].apply(lambda x: sample_neg(x)).values
+
+    return list(users), list(pos_items), list(neg_items)
+
+
+optimizer = torch.optim.Adam(lightGCN.parameters(), lr=0.005)
+
+EPOCHS = 30
+BATCH_SIZE = 1024
+DECAY = 0.0001
+K = 10
+
+loss_list_epoch = []
+MF_loss_list_epoch = []
+reg_loss_list_epoch = []
+
+recall_list = []
+precision_list = []
+ndcg_list = []
+map_list = []
+
+train_time_list = []
+eval_time_list = []
+
+for epoch in tqdm(range(EPOCHS)):
+    n_batch = int(len(train_df) / BATCH_SIZE)
+
+    final_loss_list = []
+    MF_loss_list = []
+    reg_loss_list = []
+
+    best_ndcg = -1
+
+    train_start_time = time.time()
+    lightGCN.train()
+    for batch_idx in range(n_batch):
+        optimizer.zero_grad()
+        users, pos_items, neg_items = data_loader(train_df, BATCH_SIZE, n_users, n_items)
+        users_emb, pos_emb, neg_emb, userEmb0, posEmb0, negEmb0 = lightGCN.forward(users, pos_items, neg_items)
+        mf_loss, reg_loss = bpr_loss(users, users_emb, pos_emb, neg_emb, userEmb0, posEmb0, negEmb0)
+        reg_loss = DECAY * reg_loss
+        final_loss = mf_loss + reg_loss
+        final_loss.backward()
+        optimizer.step()
+
+        final_loss_list.append(final_loss.item())
+        MF_loss_list.append(mf_loss.item())
+        reg_loss_list.append(reg_loss.item())
+
+    train_end_time = time.time()
+    train_time = train_end_time - train_start_time
+    lightGCN.eval()
+    with (torch.no_grad()):
+        final_user_Embed, final_item_Embed, initial_user_Embed, initial_item_Embed = lightGCN.propagate_through_layers()
+        test_topK_recall, test_topK_precision, test_topK_ndcg, test_topK_map = get_metrics(final_user_Embed, final_item_Embed, n_users,n_items, train_df, test_df, K)
+
+    if test_topK_ndcg > best_ndcg:
+        best_ndcg = test_topK_ndcg
+        torch.save(final_user_Embed, 'final_user_Embed.pt')
+        torch.save(final_item_Embed, 'final_item_Embed.pt')
+        torch.save(initial_user_Embed, 'initial_user_Embed.pt')
+        torch.save(initial_item_Embed, 'initial_item_Embed.pt')
+
+    eval_time = time.time() - train_end_time
+
+    loss_list_epoch.append(round(np.mean(final_loss_list), 4))
+    MF_loss_list_epoch.append(round(np.mean(MF_loss_list), 4))
+    reg_loss_list_epoch.append(round(np.mean(reg_loss_list), 4))
+    recall_list.append(round(test_topK_recall, 4))
+    precision_list.append(round(test_topK_precision, 4))
+    ndcg_list.append(round(test_topK_ndcg, 4))
+    map_list.append(round(test_topK_map, 4))
+    train_time_list.append(train_time)
+    eval_time_list.append(eval_time)
+
+epoch_list = [(i+1) for i in range(EPOCHS)]
+
+plt.plot(epoch_list, recall_list, label='Recall')
+plt.plot(epoch_list, precision_list, label='Precision')
+plt.plot(epoch_list, ndcg_list, label='NDCG')
+plt.plot(epoch_list, map_list, label='MAP')
+plt.xlabel('Epoch')
+plt.ylabel('Metrics')
+plt.legend()
+
+plt.plot(epoch_list, loss_list_epoch, label='Total Training Loss')
+plt.plot(epoch_list, MF_loss_list_epoch, label='MF Training Loss')
+plt.plot(epoch_list, reg_loss_list_epoch, label='Reg Training Loss')
+
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+
+print("Average time taken to train an epoch -> ", round(np.mean(train_time_list),2), " seconds")
+print("Average time taken to eval an epoch -> ", round(np.mean(eval_time_list),2), " seconds")
+
+print("Last Epoch's Test Data Recall -> ", recall_list[-1])
+print("Last Epoch's Test Data Precision -> ", precision_list[-1])
+print("Last Epoch's Test Data NDCG -> ", ndcg_list[-1])
+print("Last Epoch's Test Data MAP -> ", map_list[-1])
+
+print("Last Epoch's Train Data Loss -> ", loss_list_epoch[-1])
